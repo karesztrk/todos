35f5a90594f391a6fa8e65fcd807849ec2eae979
Károly Török <torok.karoly.krisztian@gmail.com>
2024-12-21 16:04:28 +0100

    SQL plugin

--- src/lib/repository/db.ts
+++ src/lib/repository/db.ts
@@ -1,28 +1,70 @@
-import { createQueries, createStore } from 'tinybase/with-schemas';
+import { createQueries, createStore, type Id } from 'tinybase/with-schemas';
+import Database from '@tauri-apps/plugin-sql';
+import { createCustomPersister } from 'tinybase/persisters/with-schemas';
 
-export const db = createStore()
-	.setTablesSchema({
-		todos: {
-			text: { type: 'string' },
-			done: { type: 'boolean' },
-			date: { type: 'string', default: undefined },
-			created: { type: 'string' }
+export const db = createStore().setTablesSchema({
+	todos: {
+		text: { type: 'string' },
+		done: { type: 'boolean' },
+		date: { type: 'string', default: undefined },
+		created: { type: 'string' }
+	}
+});
+
+export type TodoTable = { [rowId: Id]: TodoRow };
+
+export interface TodoRow {
+	text: string;
+	done: boolean;
+	date?: Date;
+	created: string;
+}
+
+let interval: number;
+
+export const persister = createCustomPersister(
+	db,
+	async () => {
+		try {
+			const database = await Database.load('sqlite:db.sqlite');
+			const res = await database.select<Todo[]>(
+				'SELECT id, text, done, date, created FROM todo ORDER BY created DESC'
+			);
+
+			const todoTable = res.reduce<any>((acc, todo) => {
+				acc[todo.id] = todo;
+				return acc;
+			}, {});
+			return [{ todos: todoTable }];
+		} catch {}
+	},
+	async (getContent, changes) => {
+		for (const t of getContent()) {
+			const todos = t.todos;
+			if (todos) {
+				const database = await Database.load('sqlite:db.sqlite');
+				for (const key in todos) {
+					const todo = todos[key];
+
+					await database.execute(
+						'INSERT into todo (id, text, done, date, created) VALUES ($1, $2, $3, $4, $5)',
+						[key, todo.text, todo.done, todo.date, todo.created]
+					);
+				}
+			}
 		}
-	})
-	.setTable('todos', {
-		'0001': {
-			text: 'Learn SvelteKit',
-			done: false,
-			date: new Date().toISOString(),
-			created: '2023-01-01'
-		},
-		'0002': {
-			text: 'Learn LightningCSS',
-			done: false,
-			date: new Date().toISOString(),
-			created: '2023-01-02'
-		}
-	});
+	},
+	(listener: TimerHandler) => (interval = setInterval(listener, 1000)),
+	() => clearInterval(interval)
+);
+persister
+	.startAutoLoad()
+	.then(() => {})
+	.catch((e) => console.error(e));
+persister
+	.startAutoSave()
+	.then(() => {})
+	.catch((e) => console.error(e));
 
 const queries = createQueries(db);
 
