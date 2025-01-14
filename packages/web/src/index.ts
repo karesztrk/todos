608445f6ac83937590ec4fd976a268f97439246c
Károly Török <torok.karoly.krisztian@gmail.com>
2025-01-14 11:38:39 +0100

    Monorepo reorganization

---
+++ packages/web/src/index.ts
@@ -0,0 +1,5 @@
+export { default as Modal } from '$lib/components/Modal';
+export { default as Todo } from '$lib/components/Todo';
+export { default as TodoDialog } from '$lib/components/TodoDialog';
+export { default as TodoList } from '$lib/components/TodoList';
+export { default as TodoView } from '$lib/components/TodoView';
